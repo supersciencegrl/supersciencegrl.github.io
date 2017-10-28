0b168e8bab761cada1608275d491eaab5860c989
supersciencegrl <methionine57@gmail.com>
2017-10-28 12:16:19 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 $(".sidebar").height(Math.max($(".content").height(),$(".sidebar").height()));
+
+  // Not gonna lie, this was copy-pasted from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Animate smooth page scroll in 250 ms
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 250, function(){
+       // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
