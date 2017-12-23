9c19e432c4acdd0a906f93b48379f620cbe582e4
supersciencegrl <methionine57@gmail.com>
2017-12-23 15:16:13 +0000

    Add files via upload

--- links/js/index.js
+++ links/js/index.js
@@ -1,3 +1,5 @@
+$(".sidebar").height(Math.max($(".content").height(),$(".sidebar").height()));
+
 function openTab(evt, tabName) {
   var i, tabContent, tabButton;
   tabContent = document.getElementsByClassName("tabContent");
@@ -13,3 +15,23 @@ function openTab(evt, tabName) {
 }
 
 document.getElementById("defaultTab").click();
+
+  // Not gonna lie, this was copy-pasted from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Animate smooth page scroll in 250 ms
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 250, function(){
+       // Add # to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
