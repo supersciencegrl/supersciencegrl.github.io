a5a22ba660e3ca74ff6fe6f25c8cd0e0a7ed0fac
supersciencegrl <methionine57@gmail.com>
2018-06-17 10:12:19 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ $(".sidebar").height(Math.max($(".content").height(),$(".sidebar").height()));
 
   // Not gonna lie, this was copy-pasted from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  // Add smooth scrolling to elements with class linkjump
+  $(".linkjump").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       event.preventDefault();
