07e4ff11ca456e4cb5e904feef1196766cb487ff
Nessa Carson <methionine57@gmail.com>
2021-05-30 13:41:04 +0100

    collapsible section testing

--- links/js/index.js
+++ links/js/index.js
@@ -60,9 +60,8 @@ $(document).ready(function(){
 
 // Collapsible
 
-var coll = document.getElementsByClassname("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
-
 for (i = 0; i < coll.length; i++) {
 	coll[i].addEventListener("click", function() {
 		this.classList.toggle("collapsible-active");
