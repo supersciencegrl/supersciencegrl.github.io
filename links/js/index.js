320120f7f2178e3b98ac582098bdc65c6aa29fd6
Nessa Carson <methionine57@gmail.com>
2021-05-30 13:30:40 +0100

    collapsible section testing

--- links/js/index.js
+++ links/js/index.js
@@ -57,3 +57,20 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+// Collapsible
+
+var coll = document.getElementsByClassname("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("collapsible-active");
+		var collapsible-content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}
