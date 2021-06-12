6ae6161c8989dad40edceb6581a52b9f54c0b111
Nessa Carson <methionine57@gmail.com>
2021-06-12 15:59:47 +0100

    Playing with js links

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
-	// Not gonna lie, this was copy-pasted from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
-	// Add smooth scrolling to elements with class linkjump
-	$(".linkjump").on('click', function(event) {
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			event.preventDefault();
-			// Store hash
+function smoothScroll() {
+	const anchors = document.querySelectorAll('.linkjump')
+	anchors.forEach(function (linkjumpClick) {
+		linkjumpClick.addEventListener('click', function (e) {
 			var hash = this.hash;
-			// Animate smooth page scroll in 250 ms
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 250, function(){
-				// Add # to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-			});
-		} // End if
-	});
+			if (hash !== "") {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 250, function() {
+					window.location.hash = hash;
+				});
+			}
+		})
+	})
+}
+			
+$(document).ready(function(){
+	// Add 'onclick' listening event for each linkjump button
+	smoothScroll();
 });
