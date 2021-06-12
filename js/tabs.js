575871809b39988200ee13ef5055e5afb8683ece
Nessa Carson <methionine57@gmail.com>
2021-06-12 15:24:38 +0100

    Neaten JS; update About page

---
+++ js/tabs.js
@@ -0,0 +1,50 @@
+function displayTabOnClick() {
+	const tabs = document.querySelectorAll('.tabButton')
+	tabs.forEach(function (tabButton) {
+		tabButton.addEventListener('click', function (e) {
+			let tabContentId = tabButton.id.split(/\-(.+)/)[1]
+			openTab(e, tabContentId);
+		})
+	})
+}
+
+function replaceUrlHash(tabName) {
+	let newUrl = `${window.location.href.split("#")[0]}#${tabName}`;
+	history.replaceState(null, null, newUrl);
+}
+
+function openTab(evt, tabName) {
+	var i, tabContent, tabButton;
+	tabContent = document.getElementsByClassName("tabContent");
+	for (i = 0; i < tabContent.length; i++) {
+		tabContent[i].style.display = "none";
+	}
+	tabButton = document.getElementsByClassName("tabButton");
+	for (i = 0; i < tabButton.length; i++) {
+		tabButton[i].className = tabButton[i].className.replace(" currentTab", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+
+	// Set new URL
+	replaceUrlHash(tabName);
+
+	evt.currentTarget.className += " currentTab";
+}
+
+function directLinkToTab() {
+	if (window.location.hash) {
+		const hash = window.location.href.split("#")[1];
+		openTab(this, hash);
+	} else {
+		const firstTab = document.querySelector('.tabButton')
+		firstTab.click();
+	}
+}
+
+$(document).ready(function(){
+	// Add 'onclick' listening event for each Tab button
+	displayTabOnClick();
+
+	// Check URL to display correct tab content
+	directLinkToTab();
+});
--- links/js/index.js
+++ links/js/index.js
@@ -1,49 +1,6 @@
-function displayTabOnClick() {
-	const tabs = document.querySelectorAll('.tabButton')
-	tabs.forEach(function (tabButton) {
-		tabButton.addEventListener('click', function (e) {
-			let tabContentId = tabButton.id.split(/\-(.+)/)[1]
-			openTab(e, tabContentId);
-		})
-	})
-}
-
-function replaceUrlHash(tabName) {
-	let newUrl = `${window.location.href.split("#")[0]}#${tabName}`;
-	history.replaceState(null, null, newUrl);
-}
-
-
-function openTab(evt, tabName) {
-	var i, tabContent, tabButton;
-	tabContent = document.getElementsByClassName("tabContent");
-	for (i = 0; i < tabContent.length; i++) {
-		tabContent[i].style.display = "none";
-	}
-	tabButton = document.getElementsByClassName("tabButton");
-	for (i = 0; i < tabButton.length; i++) {
-		tabButton[i].className = tabButton[i].className.replace(" currentTab", "");
-	}
-	document.getElementById(tabName).style.display = "block";
-
-	// Set new URL
-	replaceUrlHash(tabName);
-
-	evt.currentTarget.className += " currentTab";
-}
-
-function directLinkToTab() {
-	if (window.location.hash) {
-		const hash = window.location.href.split("#")[1];
-		openTab(this, hash);
-	} else {
-		const firstTab = document.querySelector('.tabButton')
-		firstTab.click();
-	}
-}
-
 function toggleCollapsibleContent() {
 	// Khoi's comment: minor code change using `forEach`, not important
+	// See ./js/tabs.js for behaviour regarding main tabs
 	const collapsibleItems = document.querySelectorAll(".collapsible,.collapsibleInline");
 	Array.from(collapsibleItems).forEach(function (element) {
 		element.addEventListener("click", function () {
@@ -59,12 +16,6 @@ function toggleCollapsibleContent() {
 }
 
 $(document).ready(function(){
-	// Add 'onclick' listening event for each Tab button
-	displayTabOnClick();
-
 	// Add listening event for collapsible content
 	toggleCollapsibleContent();
-
-	// Check URL to display correct tab content
-	directLinkToTab();
 });
