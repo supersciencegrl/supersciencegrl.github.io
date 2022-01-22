256af0ed312b115ef519df9f1392611e01a4c7a3
Nessa Carson <methionine57@gmail.com>
2022-01-22 13:44:39 +0000

    Disable checkbox when hash present

--- js/conference-database.js
+++ js/conference-database.js
@@ -1,72 +1,83 @@
 function pricelist() {
-  var standard = document.getElementById("standard-price");
-  var student = document.getElementById("student-price");
-  var academic = document.getElementById("academic-price");
-  var standardOutput = document.getElementsByClassName("standard");
-  var studentOutput = document.getElementsByClassName("student");
-  var academicOutput = document.getElementsByClassName("academic");
-  var i;
-  if (standard.checked) {
-    for (i = 0; i < standardOutput.length; i++) {
-    standardOutput[i].style.display = "table-cell";
-  }
-  	for (i = 0; i < studentOutput.length; i++) {
-    studentOutput[i].style.display = "none";
-  }
-  	for (i = 0; i < academicOutput.length; i++) {
-    academicOutput[i].style.display = "none";
-  }}
-  else if (student.checked) {
-    for (i = 0; i < standardOutput.length; i++) {
-    standardOutput[i].style.display = "none";
-  }
-  	for (i = 0; i < studentOutput.length; i++) {
-    studentOutput[i].style.display = "table-cell";
-  }
-  	for (i = 0; i < academicOutput.length; i++) {
-    academicOutput[i].style.display = "none";
-  }}
-  else if (academic.checked) {
-    for (i = 0; i < standardOutput.length; i++) {
-    standardOutput[i].style.display = "none";
-  }
-  	for (i = 0; i < studentOutput.length; i++) {
-    studentOutput[i].style.display = "none";
-  }
-  	for (i = 0; i < academicOutput.length; i++) {
-    academicOutput[i].style.display = "table-cell";
-  }}
-  else {
-    for (i = 0; i < standardOutput.length; i++) {
-    standardOutput[i].style.display = "table-cell";
-  }
-  	for (i = 0; i < studentOutput.length; i++) {
-    studentOutput[i].style.display = "none";
-  }
-  	for (i = 0; i < academicOutput.length; i++) {
-    academicOutput[i].style.display = "none";
-  }}
+	var standard = document.getElementById("standard-price");
+	var student = document.getElementById("student-price");
+	var academic = document.getElementById("academic-price");
+	var standardOutput = document.getElementsByClassName("standard");
+	var studentOutput = document.getElementsByClassName("student");
+	var academicOutput = document.getElementsByClassName("academic");
+	var i;
+	if (standard.checked) {
+		for (i = 0; i < standardOutput.length; i++) {
+			standardOutput[i].style.display = "table-cell";
+		}
+		for (i = 0; i < studentOutput.length; i++) {
+			studentOutput[i].style.display = "none";
+		}
+		for (i = 0; i < academicOutput.length; i++) {
+			academicOutput[i].style.display = "none";
+		}
+	}
+	else if (student.checked) {
+		for (i = 0; i < standardOutput.length; i++) {
+			standardOutput[i].style.display = "none";
+		}
+		for (i = 0; i < studentOutput.length; i++) {
+			studentOutput[i].style.display = "table-cell";
+		}
+		for (i = 0; i < academicOutput.length; i++) {
+			academicOutput[i].style.display = "none";
+		}
+	}
+	else if (academic.checked) {
+		for (i = 0; i < standardOutput.length; i++) {
+			standardOutput[i].style.display = "none";
+		}
+		for (i = 0; i < studentOutput.length; i++) {
+			studentOutput[i].style.display = "none";
+		}
+		for (i = 0; i < academicOutput.length; i++) {
+			academicOutput[i].style.display = "table-cell";
+		}
+	}
+	else {
+		for (i = 0; i < standardOutput.length; i++) {
+			standardOutput[i].style.display = "table-cell";
+		}
+		for (i = 0; i < studentOutput.length; i++) {
+			studentOutput[i].style.display = "none";
+		}
+		for (i = 0; i < academicOutput.length; i++) {
+			academicOutput[i].style.display = "none";
+		}
+	}
 }
 
 function removecancelled() {
-  var box = document.getElementById("cancelled-checkbox");
-  var cancelledOutput = document.getElementsByClassName("cancelled");
-  var postponedOutput = document.getElementsByClassName("postponed");
-  var i;
-  if (box.checked) {
-	for (i = 0; i < cancelledOutput.length; i++) {
-	cancelledOutput[i].style.display = "none";
+	var box = document.getElementById("cancelled-checkbox");
+	var cancelledOutput = document.getElementsByClassName("cancelled");
+	var postponedOutput = document.getElementsByClassName("postponed");
+	var i;
+	if (!(window.location.hash)) {
+		if (box.checked) {
+			for (i = 0; i < cancelledOutput.length; i++) {
+				cancelledOutput[i].style.display = "none";
+			}
+			for (i = 0; i < postponedOutput.length; i++) {
+				postponedOutput[i].style.display = "none";
+			}
+		}
+		else {
+			for (i = 0; i < cancelledOutput.length; i++) {
+				cancelledOutput[i].style.display = "table-row";
+			}
+			for (i = 0; i < postponedOutput.length; i++) {
+				postponedOutput[i].style.display = "table-row";
+			}
+		}
 	}
-	for (i = 0; i < postponedOutput.length; i++) {
-	postponedOutput[i].style.display = "none";
-  }}
-  else {
-	for (i = 0; i < cancelledOutput.length; i++) {
-	cancelledOutput[i].style.display = "table-row";
+	else {
+		box.disabled = true;
 	}
-	for (i = 0; i < postponedOutput.length; i++) {
-	postponedOutput[i].style.display = "table-row";
-  }}
 }
 
 function directLinkToSubset() {
@@ -103,6 +114,14 @@ function directLinkToSubset() {
 			}
 		}
 	}
+}
+
+function removeFilter() {
+	allContent = document.getEleemtnsByClassName("body");
+	for (i = 0; i < allContent.length; i++) {
+		allContent[i].style.display = "table-row";
+	};
+	history.pushState("", document.title, window.location.pathname + window.location.search);
 }
 
 function replaceUrlHash(tabName) {
