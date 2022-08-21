bca77ed0593c448c462eac48d5dc870ff1aff48a
Nessa Carson <methionine57@gmail.com>
2022-08-21 18:49:12 +0100

    git add .

--- js/learning.js
+++ js/learning.js
@@ -22,34 +22,39 @@ function removeLevelFilter() {
 function filterLearning() {
 	var i;
 	learningAllCheckbox.checked = false;
-	if (learningIntroCheckbox.checked) {
-		for (i = 0; i < introResources.length; i++) {
-			introResources[i].style.display = "block";
-		}
+	if (!learningAllCheckbox.checked && !learningIntroCheckbox.checked && !learningAdvCheckbox.checked && !learningSpecialistCheckbox.checked) {
+		removeLevelFilter()
 	}
 	else {
-		for (i = 0; i < introResources.length; i++) {
-			introResources[i].style.display = "none";
+		if (learningIntroCheckbox.checked) {
+			for (i = 0; i < introResources.length; i++) {
+				introResources[i].style.display = "block";
+			}
 		}
-	}
-	if (learningAdvCheckbox.checked) {
-		for (i = 0; i < advResources.length; i++) {
-			advResources[i].style.display = "block";
+		else {
+			for (i = 0; i < introResources.length; i++) {
+				introResources[i].style.display = "none";
+			}
 		}
-	}
-	else {
-		for (i = 0; i < advResources.length; i++) {
-			advResources[i].style.display = "none";
+		if (learningAdvCheckbox.checked) {
+			for (i = 0; i < advResources.length; i++) {
+				advResources[i].style.display = "block";
+			}
 		}
-	}
-	if (learningSpecialistCheckbox.checked) {
-		for (i = 0; i < specialistResources.length; i++) {
-			specialistResources[i].style.display = "block";
+		else {
+			for (i = 0; i < advResources.length; i++) {
+				advResources[i].style.display = "none";
+			}
 		}
-	}
-	else {
-		for (i = 0; i < specialistResources.length; i++) {
-			specialistResources[i].style.display = "none";
+		if (learningSpecialistCheckbox.checked) {
+			for (i = 0; i < specialistResources.length; i++) {
+				specialistResources[i].style.display = "block";
+			}
+		}
+		else {
+			for (i = 0; i < specialistResources.length; i++) {
+				specialistResources[i].style.display = "none";
+			}
 		}
 	}
 }
