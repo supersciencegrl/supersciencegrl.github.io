bc5fc294ebe57af4254c870b6a5b29102f8d2c28
Nessa Carson <methionine57@gmail.com>
2023-01-22 16:32:08 +0000

    20230122 update

--- update-batch.py
+++ update-batch.py
@@ -1,6 +1,8 @@
 import glob
 
 def checksublist(mylist, snippet):
+    ''' Checks where the html snippet exists in the readlines '''
+    
     listlength = len(mylist)
     snippetlength = len(snippet)
     idxlist = [i for i in range(listlength-snippetlength+1) if snippet == mylist[i:i+snippetlength]]
@@ -8,10 +10,39 @@ def checksublist(mylist, snippet):
     return idxlist
 
 def inputToHtml(myinput):
+    ''' Formats the inputs into an html list with newlines at the end of each line '''
+    
     mylist = myinput.split('\\n')
     editedlist = [i+'\n' for i in mylist]
 
     return editedlist
+
+def run(pages, oldhtml, newhtml):
+    ''' Converts the old to the new html in all webpages and provides a printed report '''
+    ''' Note: BeautifulSoup etc does not maintain indentation '''
+    
+    pageschanged = 0
+    pagesnotchanged = []
+    for page in pages:
+        with open(page, 'r+', encoding='utf8') as fin:
+            html = fin.readlines()
+
+            idxlist = checksublist(html, oldhtml)
+            for idx in idxlist[::-1]:
+                html[idx:idx+oldlength] = newhtml
+            if idxlist:
+                print(f'{page}:\t{len(idxlist)} instances of old html replaced.')
+                pageschanged += 1
+            else:
+                pagesnotchanged.append(page)
+
+            fin.seek(0)
+            fin.writelines(html)
+            fin.truncate() # Allows files to be shortened without leaving lines at the bottom
+
+    print(f'\n{pageschanged} pages changed.')
+    pagesnotchanged_string = ('\n').join(pagesnotchanged)
+    print(f'Pages not changed ({len(pagesnotchanged)}):\n', pagesnotchanged_string)
 
 pages = glob.glob('**/*.html', recursive=True)
 exclusions = ['googlec9a765a08c18ee51.html', 'pinterest-fc74e.html']
@@ -20,10 +51,14 @@ for exc in exclusions:
     _ = pages.pop(idx)
 
 print('WARNING: Recommend git push before editing for easy reversion to original.\n')
-oldhtml = input('Old html (include \\t; replace newlines with \'\\n\'): ')
+oldhtml = ''
+while not oldhtml:
+    oldhtml = input('Old html (copy tabs as tabs without changing; replace newlines with \'\\n\'): ')
 oldhtml = inputToHtml(oldhtml)
 
-newhtml = input('New html (include \\t; replace newlines with \'\\n\'): ')
+newhtml = ''
+while not newhtml:
+    newhtml = input('New html (copy tabs as tabs without changing; replace newlines with \'\\n\'): ')
 newhtml = inputToHtml(newhtml)
 
 # Example html
@@ -39,25 +74,4 @@ newhtml = inputToHtml(newhtml)
 oldlength = len(oldhtml)
 newlength = len(newhtml)
 
-#''' Note: BeautifulSoup etc does not maintain indentation '''
-pageschanged = 0
-pagesnotchanged = []
-for page in pages:
-    with open(page, 'r+', encoding='utf8') as fin:
-        html = fin.readlines()
-
-        idxlist = checksublist(html, oldhtml)
-        for idx in idxlist[::-1]:
-            html[idx:idx+oldlength] = newhtml
-        if idxlist:
-            print(f'{page}:\t{len(idxlist)} instances of old html replaced.')
-            pageschanged += 1
-        else:
-            pagesnotchanged.append(page)
-
-        fin.seek(0)
-        fin.writelines(html)
-
-print(f'\n{pageschanged} pages changed.')
-pagesnotchanged_string = ('\n').join(pagesnotchanged)
-print(f'Pages not changed ({len(pagesnotchanged)}):\n', pagesnotchanged_string)
+run(pages, oldhtml, newhtml)
