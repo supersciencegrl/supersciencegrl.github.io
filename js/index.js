95a258d6416f1e60bd8b26c5ee94e451631cfc37
Nessa Carson <methionine57@gmail.com>
2023-03-26 18:00:23 +0100

    Copy code buttons

--- js/index.js
+++ js/index.js
@@ -22,10 +22,27 @@ function eatHamburger() {
 		ham.classList.toggle("menuActive");
 	})
 }
+
+function addCopyCodeButtons() {
+	let blocks = document.querySelectorAll("pre");
+	blocks.forEach((block) => {
+		// Only add button if browser supports Clipboard API
+		if (navigator.clipboard) {
+			let button = document.createElement("button");
+			button.innerHTML = "<i class="fa-regular fa-copy" aria-label="Copy code"></i>";
+			block.appendChild(button);
+			button.addEventListener("click", async () => {
+				await copyCode(block);
+			});
+		}
+	});
+}
 	
 $(document).ready(function(){
 	// Add 'onclick' listening event for each linkjump button
 	smoothScroll();
 	// Add 'onclick' listening event for mobile hamburger menu
 	eatHamburger();
+	// Add 'copy code' buttons to all pre elements with code children
+	addCopyCodeButtons();
 });
